59e7dcdf3dd23f531dea07b29e54f4ad2e5866cb
James Clarke <james@edgedb.com>
2025-05-01 18:03:28 +0100

    Add logging of api requests (#59)

--- pages-common/apiHandler.ts
+++ pages-common/apiHandler.ts
@@ -1,6 +1,7 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {handleExceptions} from "./handleExceptions";
 import {AuthContext, readJWTBearer} from "./auth";
+import {wrapAPILogging} from "./logger";
 
 type APIHandler = (req: NextApiRequest, res: NextApiResponse) => Promise<void>;
 
@@ -12,6 +13,7 @@ export function createAPIHandler(methodHandlers: {
   PATCH?: MethodHandler;
   PUT?: MethodHandler;
   DELETE?: MethodHandler;
+  _all?: MethodHandler;
 }): APIHandler {
   const handlers: {[key: string]: {handler: APIHandler; noAuth: boolean}} = {};
   for (const [method, handler] of Object.entries(methodHandlers)) {
@@ -21,34 +23,35 @@ export function createAPIHandler(methodHandlers: {
         : {...handler, noAuth: handler.noAuth ?? false};
   }
 
-  return async (req: NextApiRequest, res: NextApiResponse) => {
-    const methodHandler = handlers[req.method!];
+  return async (req: NextApiRequest, res: NextApiResponse) =>
+    wrapAPILogging(req, res, async () => {
+      const methodHandler = handlers[req.method!] ?? handlers._all;
 
-    if (!methodHandler) {
-      res.status(405).end(`method ${req.method} not allowed`);
-      return;
-    }
-
-    // Auth can be disabled on a per method basis because some admin api
-    // routes can still be public:
-    // for example those that return the list of agreements (GET),
-    // even though cherry-picking endpoints that require authentication is
-    // not the best developer's UX.
-    if (!methodHandler.noAuth) {
-      /**
-       * Requires an authenticated user for an API request. Inspects request
-       * headers to look for a valid JWT Bearer token. Stops request handling
-       * and returns 401 for unauthorized users.
-       */
-      try {
-        const user = await readJWTBearer(req);
-        (req as unknown as AuthContext).user = user;
-      } catch (err: any) {
-        res.status(401).end(err);
+      if (!methodHandler) {
+        res.status(405).end(`method ${req.method} not allowed`);
         return;
       }
-    }
 
-    await handleExceptions(res, () => methodHandler.handler(req, res));
-  };
+      // Auth can be disabled on a per method basis because some admin api
+      // routes can still be public:
+      // for example those that return the list of agreements (GET),
+      // even though cherry-picking endpoints that require authentication is
+      // not the best developer's UX.
+      if (!methodHandler.noAuth) {
+        /**
+         * Requires an authenticated user for an API request. Inspects request
+         * headers to look for a valid JWT Bearer token. Stops request handling
+         * and returns 401 for unauthorized users.
+         */
+        try {
+          const user = await readJWTBearer(req);
+          (req as unknown as AuthContext).user = user;
+        } catch (err: any) {
+          res.status(401).end(err);
+          return;
+        }
+      }
+
+      await handleExceptions(res, () => methodHandler.handler(req, res));
+    });
 }
